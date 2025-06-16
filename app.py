cb1d13fb80044c17993644c334401756bf06a997
JxWayne890 <JxWayne890@gmail.com>
2025-06-16 15:29:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import sys
 # Add the project root to the Python path
 sys.path.insert(0, os.path.dirname(__file__))
 
-from src.main import app
+from main import app
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
